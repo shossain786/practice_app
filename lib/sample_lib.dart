740bb689b0812825602db9a0d0c352cc6314c78b
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-05-13 22:42:02 +0530

    code updated

---
+++ lib/sample_lib.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MySampleLib extends StatelessWidget {
+  const MySampleLib({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SAMPLE APP'),
+      ),
+    );
+  }
+}
